9921454eaff644fd47feeeb6891267b7586f2f5e
untael <pashamironow@gmail.com>
2020-05-12 16:56:53 +0300

    fix: minor

--- generator/index.js
+++ generator/index.js
@@ -7,9 +7,8 @@ module.exports = (api, opts) => {
       const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' })
       const lines = contentMain.split(/\r?\n/g)
       const renderIndex = lines.findIndex(line => line.match(/new Vue/))
-      const importedString = lines[renderIndex - 1] !== EOL ? [EOL, 'consoleBuildInfo()', EOL] : ['consoleBuildInfo()', EOL]
-    ]
-      lines[renderIndex - 1] += importedString.join(EOL)
+      const importedStrings = lines[renderIndex - 1] !== EOL ? [EOL, 'consoleBuildInfo()', EOL] : ['consoleBuildInfo()', EOL]
+      lines[renderIndex - 1] += importedStrings.join(EOL)
 
       fs.writeFileSync(api.entryFile, lines.join(EOL), { encoding: 'utf-8' })
     })
