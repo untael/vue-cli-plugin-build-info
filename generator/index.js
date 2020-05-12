9bdb11d7732aab68ef73fb74ad2a8e596cc6937b
untael <pashamironow@gmail.com>
2020-05-12 16:54:52 +0300

    fix: generator imports

--- generator/index.js
+++ generator/index.js
@@ -6,13 +6,14 @@ module.exports = (api, opts) => {
       const fs = require('fs')
       const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' })
       const lines = contentMain.split(/\r?\n/g)
-      const importedString = [EOL, 'consoleBuildInfo()', EOL]
       const renderIndex = lines.findIndex(line => line.match(/new Vue/))
+      const importedString = lines[renderIndex - 1] !== EOL ? [EOL, 'consoleBuildInfo()', EOL] : ['consoleBuildInfo()', EOL]
+    ]
       lines[renderIndex - 1] += importedString.join(EOL)
 
       fs.writeFileSync(api.entryFile, lines.join(EOL), { encoding: 'utf-8' })
     })
 
-    api.injectImports(api.entryFile, `import { consoleBuildInfo } from 'vue-cli-plugin-build-info/app'`)
+    api.injectImports(api.entryFile, `import { consoleBuildInfo } from 'vue-cli-plugin-build-info/plugin'`)
   }
 }
